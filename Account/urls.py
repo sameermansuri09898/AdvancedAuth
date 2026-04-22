56293702b772d9cdcf787a2d1c2907670be60ab4
sameermansuri09898 <sameermansuri09898@gmail.com>
2026-04-22 22:32:17 +0530

    update url

--- Account/urls.py
+++ Account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import register,login,logout,VericationOtp,ResendOtp
+from .views import register,login,logout,VericationOtp,ResendOtp,ChangePasswordView
 urlpatterns = [
     path('register/',register.as_view(),name='register'),
     path('login/',login.as_view(),name='login'),
     path('logout/',logout.as_view(),name='logout'),
     path('verifyotp/',VericationOtp.as_view(),name='verifyotp'),
     path('resendotp/',ResendOtp.as_view(),name='resendotp'),
+    path('change-password/',ChangePasswordView.as_view(),name='change-password'),
   
 
 ]
